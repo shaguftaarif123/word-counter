fdae6b265c6549d3b4062db2e0220df6331172cf
shaguftaarif123 <143534829+shaguftaarif123@users.noreply.github.com>
2023-11-08 11:28:25 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+function counter(paragraph) {
+    let freeWhiteSpace = paragraph.replace(/\s/g, "");
+    return freeWhiteSpace.length;
+}
+async function startWordCount(counter) {
+    do {
+        let res = await inquirer.prompt({
+            type: "input",
+            message: "write paragraph here...",
+            name: "paragraph"
+        });
+        console.log(counter(res.paragraph));
+    } while (true);
+}
+startWordCount(counter);
---
+++ index.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+
+function counter(paragraph:string){
+    let freeWhiteSpace = paragraph.replace(/\s/g,"")
+
+    return freeWhiteSpace.length
+}
+
+ async function startWordCount(counter:(text:string)=> number){
+   do {let res = await inquirer.prompt({
+        type: "input",
+        message: "write paragraph here...",
+        name: "paragraph"
+    })
+
+   console.log(counter(res.paragraph))
+}
+while(true)
+ }
+
+startWordCount(counter)
